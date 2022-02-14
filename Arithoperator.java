02cd1757315a0e5c00e86bff3847580fa55d0b47
Prakash086 <ppudasaini086@gmail.com>
2022-02-14 12:59:38 +0545

    code pushed

---
+++ Arithoperator.java
@@ -0,0 +1,11 @@
+package arithmaticOperator;
+
+public class Arithoperator {
+
+    public void add(){
+        int a = 2;
+        int b = 2;
+        int sum = a + b;
+        System.out.println("Sum of Two Number is :" + sum);
+    }
+}
---
+++ Outartihmatic.java
@@ -0,0 +1,10 @@
+package arithmaticOperator;
+
+public class Outartihmatic {
+    public static void main (String [ ] args) {
+        Arithoperator var = new Arithoperator();
+        var.add();
+
+    }
+
+}
